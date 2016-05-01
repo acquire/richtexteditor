33454c4dce40f90d1bff7138ef8e90180bd2b94b
acquire <acquire@users.noreply.github.com>
2016-04-30 23:34:11 -0700

    Added files via upload

---
+++ richtexteditor/richtexteditor/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace richtexteditor
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+        Feature list
+        ------------
+        Heading Style
+        Prompt to capitalize sentences
+        Dictionary
+        Spell Check
+        Title is filename
+        show whitespace characters
+        */
+
+        /*
+        bugs
+        ----
+        save cancel crashes program
+        */
+
+        string filename = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            //save
+            //filename = Prompt.ShowDialog("Save", "Enter a filename", ".rtf");
+            //richTextBox1.SaveFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\" + filename);
+
+            //open
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //open
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            ApplyStyle();
+        }
+
+        void ApplyStyle()
+        {
+            FontStyle newStyle = FontStyle.Regular;
+            if (toolStripButton3.Checked) { newStyle = FontStyle.Bold; }
+            if (toolStripButton4.Checked) { newStyle = newStyle | FontStyle.Italic; }
+            if (toolStripButton5.Checked) { newStyle = newStyle | FontStyle.Underline; }
+            if (toolStripButton6.Checked) { newStyle = newStyle | FontStyle.Strikeout; }
+            richTextBox1.SelectionFont = new Font(richTextBox1.Font, newStyle);
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily, richTextBox1.SelectionFont.Size + 10, richTextBox1.SelectionFont.Style);
+        }
+    }
+}
---
+++ richtexteditor/richtexteditor/Prompt.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Michael_Huynh2
+ * Date: 22/04/2016
+ * Time: 7:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace richtexteditor
+{
+	public static class Prompt
+{
+    /// <summary>
+    /// Displays a prompt dialog and returns a value.
+    /// </summary>
+    /// <returns>Returns user input value. If user enters nothing empty string is returned.</returns>
+    public static string ShowDialog()
+    {
+        return ShowDialog("Prompt", "Input value: ", "");
+    }
+
+    /// <summary>
+    /// Displays a prompt dialog and returns a value.
+    /// </summary>
+    /// <param name="title">Text to be shown in the windowbar</param>
+    /// <param name="message">Message to get user to input a value</param>
+    /// <returns>Returns user input value. If user enters nothing empty string is returned.</returns>
+    public static string ShowDialog(string title, string message)
+    {
+        return ShowDialog(title, message, "");
+    }
+
+    /// <summary>
+    /// Displays a prompt dialog and returns a value.
+    /// </summary>
+    /// <param name="title">Text to be shown in the windowbar</param>
+    /// <param name="message">Message to get user to input a value</param>
+    /// <param name="defaultValue">The default value to assist user input</param>
+    /// <returns>Returns user input value. If user enters nothing empty string is returned.</returns>
+    public static string ShowDialog(string title, string message, string defaultValue)
+    {
+        //Create controls and set default values
+        Form dialog = new Form() { Width = 300, Height = 129, FormBorderStyle = FormBorderStyle.FixedDialog, Text = title, StartPosition = FormStartPosition.CenterScreen };
+        Label label1 = new Label() { Left = 10, Top = 10 };
+        TextBox textBox1 = new TextBox() { Left = 10, Top = 30, Width = 260, Height = 20 };
+        Button button1 = new Button() { Text = "Ok", Left = 116, Top = 59, Width = 75, Height = 23 };
+        Button button2 = new Button() { Text = "Cancel", Left = 197, Top = 59, Width = 75, Height = 23 };
+
+        //Add all the creations to dialog
+        dialog.Controls.Add(textBox1);
+        dialog.Controls.Add(label1);
+        dialog.Controls.Add(button1);
+        dialog.Controls.Add(button2);
+
+        //Set behaviours of new controls
+        button1.Click += (sender, e) => { dialog.Close(); };
+        button1.DialogResult = DialogResult.OK;
+        button2.Click += (sender, e) => { dialog.Close(); };
+        dialog.AcceptButton = button1; //press enter to accept
+        label1.Text = message; //prompt the user to type something
+        label1.AutoSize = true; //incase text is longer than label, text don't get chopped off
+        textBox1.Text = defaultValue;
+
+        //If ok is pressed, return the user input text, else return empty string
+        return dialog.ShowDialog() == DialogResult.OK ? textBox1.Text : "";
+    }
+	}
+}
